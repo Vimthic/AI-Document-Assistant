e0ecd5ca24b87a44c47f80bc5120d7dc66f4dca4
vimthic <vimthic22@gmail.com>
2026-07-16 14:53:04 +0530

    Added Streamlit web interface

--- app/file_reader.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-def read_text_file(file_path):
-    path = Path(file_path)
-
-    with path.open("r", encoding="utf-8") as file:
-        return file.read()
--- app/preprocessing.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def clean_text(text):
-    text = text.lower()
-    text = text.replace("\n", " ")
-    text = re.sub(r"\s+", " ", text)
-    text = re.sub(r"[^\w\s.]", "", text)
-    return text.strip()
